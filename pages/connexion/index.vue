add08225a1a1f598f057427a384e09733b458f9e
Riri-Z <riri.zamor@gmail.com>
2024-09-13 12:34:55 +0200

    Feat : login form validation

--- pages/connexion/index.vue
+++ pages/connexion/index.vue
@@ -1,10 +1,43 @@
 <script setup lang="ts">
+import { required, email, minLength, helpers } from '@vuelidate/validators';
+
+import { useVuelidate } from '@vuelidate/core';
+
 definePageMeta({
   layout: 'connexion',
 });
 
-const email = ref('');
-const password = ref('');
+const formData = reactive({
+  email: '',
+  password: '',
+});
+
+const rules = computed(() => {
+  return {
+    email: {
+      required: helpers.withMessage('Un email est requis', required),
+      email: helpers.withMessage('Email invalide', required),
+    },
+    password: {
+      required: helpers.withMessage('Le mot de passe est requis', required),
+      minLength: helpers.withMessage(
+        'Le mot de passe doit être composé de 6 caractères minimum',
+        minLength(6),
+      ),
+    },
+  };
+});
+const v$ = useVuelidate(rules, formData);
+
+const submitForm = () => {
+  v$.value.$validate();
+  if (!v$.value.$error) {
+    console.log('not error', v$.value.$error);
+  } else {
+    console.log('   v$', v$);
+    console.log('error', v$.value.$error);
+  }
+};
 </script>
 
 <template>
@@ -16,38 +49,71 @@ const password = ref('');
         inscrivez-vous ici
       </span>
     </p>
-    <form class="w-100 flex max-w-[400px] flex-col gap-6" @submit.prevent>
+    <form
+      class="w-100 flex max-w-[400px] flex-col gap-6"
+      @submit.prevent="submitForm"
+    >
       <label class="pl-1 text-base font-medium leading-5" for="email"
         >Email</label
       >
       <input
-        class="border-1 h-10 w-full rounded-md border-gray-300 bg-button-color px-3 py-2 text-sm text-primary-white-color placeholder-white outline-none focus:border-button-color focus:shadow-box-shadow-color"
-        v-model="email"
+        class="autofill-bg border-1 h-10 w-full rounded-md border-gray-300 bg-button-color px-3 py-2 text-sm text-primary-white-color placeholder-white outline-none autofill:bg-button-color focus:border-button-color focus:shadow-box-shadow-color"
+        :class="{
+          'border-2 border-red-500 focus:border-red-500': v$.email.$error,
+          'border-2 border-green-300 focus:border-green-300':
+            !v$.email.$invalid,
+        }"
+        v-model="formData.email"
         type="email"
         id="email"
-        formControlName="email"
-        placeholder="Entrez votre email"
+        placeholder="Entrez votre email e.g. example@email.com"
+        @change="v$.email.$touch"
+        required
       />
       <label class="pl-1 text-base font-medium leading-5" for="password"
         >Mot de passe</label
       >
-      <input
-        class="border-1 h-10 w-full rounded-md border-gray-300 bg-button-color px-3 py-2 text-sm text-primary-white-color placeholder-white outline-none focus:border-button-color focus:shadow-box-shadow-color"
-        v-model="password"
-        type="password"
-        id="password"
-        formControlName="password"
-        placeholder="Entrez votre mot de passe"
-      />
+      <div class="flex flex-col">
+        <input
+          class="autofill-bg border-1 h-10 w-full rounded-md border-gray-300 bg-button-color px-3 py-2 text-sm text-primary-white-color placeholder-white outline-none autofill:bg-button-color focus:border-button-color focus:shadow-box-shadow-color"
+          :class="{
+            'border-2 border-red-500 focus:border-red-500': v$.password.$error,
+            'border-2 border-green-300 focus:border-green-300':
+              !v$.password.$invalid,
+          }"
+          v-model="formData.password"
+          type="password"
+          id="password"
+          placeholder="Entrez votre mot de passe"
+          @change="v$.password.$touch"
+          required
+        />
+        <div class="mt-1 h-5">
+          <span
+            class="relative text-xs text-red-500"
+            v-if="v$.password.$error"
+            >{{ v$.password.$errors[0].$message }}</span
+          >
+        </div>
+      </div>
       <a href="#" class="cursor-pointer underline underline-offset-4"
         >Mot de passe oublié ?</a
       >
+
       <button
         class="h-10 rounded-md bg-button-color hover:shadow-box-shadow-color"
         type="submit"
       >
-        Continuer
+        Connexion
       </button>
     </form>
   </div>
 </template>
+
+<style lang="scss" scoped>
+/* overrie the default autofill style */
+.autofill-bg:-webkit-autofill {
+  -webkit-text-fill-color: white !important;
+  box-shadow: inset 0 0 0 1000px #9370db !important;
+}
+</style>
