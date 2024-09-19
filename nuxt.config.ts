2cc7d08ffa7c4fca9364751da7d9335268c9cf32
Riri-Z <riri.zamor@gmail.com>
2024-09-19 17:46:21 +0200

    Chore : add nuxt-auth-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/styles/main.css'],
+  css: ["~/assets/styles/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -10,11 +10,11 @@ export default defineNuxtConfig({
     },
   },
   modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/i18n',
-    '@nuxt/eslint',
-    '@nuxt/image',
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "nuxt-auth-utils",
   ],
-
-})
+});
