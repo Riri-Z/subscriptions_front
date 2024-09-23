6db98561b3566d2b565736a0c951a3a6348cacf0
Riri-Z <riri.zamor@gmail.com>
2024-09-23 21:16:24 +0200

    feat : add toggle type password

--- components/FormInput.vue
+++ components/FormInput.vue
@@ -3,24 +3,34 @@
     <label class="block text-sm font-medium text-gray-700" :for="id">{{
       label
     }}</label>
-    <input
-      class="autofill-bg placeHolder-gray-400 my-2 w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-2"
-      :class="{
-        'border-gray-300 focus:border-purple-500 focus:ring-purple-500':
-          !isError && modelValue.length === 0,
-        'border-red-500 focus:border-red-500 focus:ring-red-500': isError,
-        'border-green-500 focus:border-green-500 focus:ring-green-500':
-          modelValue.length > 0 && !isError,
-      }"
-      :id="id"
-      :type="type"
-      :placeHolder="placeHolder"
-      :required="required"
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-      "
-    />
 
+    <div class="flex flex-row gap-2 align-middle">
+      <input
+        class="autofill-bg flex-grow-1 placeHolder-gray-400 my-2 w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-2"
+        :class="{
+          'border-gray-300 focus:border-purple-500 focus:ring-purple-500':
+            !isError && modelValue.length === 0,
+          'border-red-500 focus:border-red-500 focus:ring-red-500': isError,
+          'border-green-500 focus:border-green-500 focus:ring-green-500':
+            modelValue.length > 0 && !isError,
+        }"
+        :id="id"
+        :type="showPassword ? 'text' : type"
+        :placeHolder="placeHolder"
+        :required="required"
+        @input="
+          $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+        "
+      />
+      <button @click="toggleTypePassword">
+        <NuxtImg
+          v-if="id === 'password'"
+          :src="showPassword ? '/icons/eye-closed.svg' : '/icons/eye-open.svg'"
+          width="24"
+          height="24"
+        />
+      </button>
+    </div>
     <p
       class="text-xs"
       :class="{
@@ -34,7 +44,7 @@
 </template>
 
 <script lang="ts" setup>
-defineProps<{
+const props = defineProps<{
   id: string;
   label: string;
   type: string;
@@ -44,6 +54,15 @@ defineProps<{
   condition: string;
   modelValue: string;
 }>();
+const showPassword = ref(false);
+
+function toggleTypePassword() {
+  showPassword.value = !showPassword.value;
+  const input = document.getElementById(props.id) as HTMLInputElement;
+  if (input) {
+    input.type = showPassword.value ? "text" : "password";
+  }
+}
 
 defineEmits(["update:modelValue"]);
 </script>
