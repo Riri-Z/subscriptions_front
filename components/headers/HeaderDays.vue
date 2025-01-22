aa40b9d0e5abf2fca91cf12919ce853a6fe09c8d
Riri-Z <riri.zamor@gmail.com>
2025-01-22 13:33:56 +0100

    Feat : extract to component

---
+++ components/headers/HeaderDays.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    v-for="dayName in NAME_DAYS"
+    :key="dayName"
+    aria-label="Day of the week"
+    class="my-1 rounded-xl bg-slate-600 py-2 text-center text-xs lg:text-sm"
+  >
+    <span>
+      {{ dayName.slice(0, 3) }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { NAME_DAYS } = useDate();
+</script>
