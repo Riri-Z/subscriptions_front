be62e38a060c79b4cfda58f44b40c8d1c9b1198d
Riri-Z <riri.zamor@gmail.com>
2025-01-23 14:44:57 +0100

    Feat : add map color for each letter

---
+++ utils/letter-color.ts
@@ -0,0 +1,28 @@
+export const letterColorMap: { [key: string]: string } = {
+  a: "#FF0000", // Rouge vif
+  b: "#0000FF", // Bleu
+  c: "#00FF00", // Vert vif
+  d: "#FFA500", // Orange
+  e: "#800080", // Violet
+  f: "#FFC0CB", // Rose clair
+  g: "#008000", // Vert foncé
+  h: "#FFD700", // Or
+  i: "#FFFFFF", // Blanc
+  j: "#4B0082", // Indigo
+  k: "#FF00FF", // Magenta
+  l: "#00FFFF", // Cyan
+  m: "#8B4513", // Marron
+  n: "#000080", // Bleu marine
+  o: "#808080", // Gris
+  p: "#800000", // Bordeaux
+  q: "#7FFF00", // Vert chartreuse
+  r: "#FF4500", // Rouge-orange
+  s: "#DA70D6", // Orchidée
+  t: "#008080", // Sarcelle
+  u: "#FA8072", // Saumon
+  v: "#9400D3", // Violet foncé
+  w: "#40E0D0", // Turquoise
+  x: "#696969", // Gris foncé
+  y: "#FFFF00", // Jaune
+  z: "#000000", // Noir
+};
