f088b993917d36107b8cc1ff845e0578b869decb
Riri-Z <riri.zamor@gmail.com>
2025-02-13 18:42:36 +0100

    Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  components: [
+    {
+      path: "~/components",
+    },
+  ],
+
   sourcemap: {
     server: true,
     client: true,
