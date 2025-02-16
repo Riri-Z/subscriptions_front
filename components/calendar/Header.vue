a9d37fc3f2a8d2f2f26fbcf546090adb4556479a
Riri-Z <riri.zamor@gmail.com>
2025-02-16 18:27:16 +0100

    Fix : Total expenses

--- components/calendar/Header.vue
+++ components/calendar/Header.vue
@@ -1,14 +1,10 @@
 <template>
   <header id="header" class="flex flex-row justify-between">
-    <div
-      id="month"
-      class="flex flex-row justify-center gap-1 align-middle md:ml-2"
-    >
+    <div id="month" class="flex flex-row justify-center gap-1 align-middle md:ml-2">
       <section class="flex justify-center align-middle">
         <div class="flex min-w-6">
           <NavMonth name-icon="chevron-left" @click="emits('prevMonth')" />
-          <NavMonth name-icon="chevron-right" @click="emits('nextMonth')">
-          </NavMonth>
+          <NavMonth name-icon="chevron-right" @click="emits('nextMonth')"> </NavMonth>
         </div>
         <section
           class="flex items-center justify-center gap-2 text-base md:text-lg lg:text-3xl"
@@ -24,10 +20,9 @@
     </div>
     <!-- SPEND AMOUNT -->
     <div id="totalSpend" class="mr-4">
-      <p class="text-sm md:text-base">Total</p>
-      <p id="sum" class="cursor-pointer text-end text-2xl">
-        {{ subscriptionStore.getTotalExpensesByMonth }} €
-      </p>
+      <p class="text-sm md:text-base">Total pour ce mois :</p>
+      <!-- TODO : internalize the currency -->
+      <p id="sum" class="cursor-pointer text-end text-2xl">{{ computeTotalExpense }} €</p>
     </div>
   </header>
   <span ref="headerDays" class="grid w-full grid-cols-7 gap-2">
@@ -48,12 +43,15 @@ const props = defineProps<{
   displayMonth: boolean;
 }>();
 
-const emits = defineEmits([
-  "prevMonth",
-  "nextMonth",
-  "clickMonth",
-  "clickYear",
-]);
-</script>
+const emits = defineEmits(["prevMonth", "nextMonth", "clickMonth", "clickYear"]);
 
-<style></style>
+/**
+ * Compute total expenses for the currentMonth
+ */
+const computeTotalExpense = computed(() => {
+  return subscriptionStore.computeTotalExpensesForCurrentMonth(
+    dateStore.getCurrentMonth,
+    subscriptionStore.subscriptionsCurrentMonth
+  );
+});
+</script>
