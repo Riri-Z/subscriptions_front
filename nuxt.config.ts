e4a2f4ac883b67cf5adc58187ed3741418b91e77
Riri-Z <riri.zamor@gmail.com>
2025-03-20 12:19:31 +0100

    Chore  : Update package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
   ],
   i18n: {
-    vueI18n: "./i18n.config.ts",
+    vueI18n: "~/i18n.config.ts",
     strategy: "no_prefix",
     locales: [
       {
